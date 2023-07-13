125c1a25c2bc8a4a02f57f1516dd7d493c27c7bc
anescdev <anescdev@gmail.com>
2023-07-13 05:12:29 +0200

    Proyecto básico

---
+++ src/test/java/es/anescdev/mcservpanel/McservpanelApplicationTests.java
@@ -0,0 +1,13 @@
+package es.anescdev.mcservpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McservpanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
